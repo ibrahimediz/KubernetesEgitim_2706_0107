a39c5843e419a829ec0d0aaba94835c4c81a1629
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2022-06-28 14:34:15 +0300

    Co-authored-by: Özlem Aslan <ozlemaslan@users.noreply.github.com> Co-authored-by: Fatih <fmetin@users.noreply.github.com> Co-authored-by: Mustafa Alan <cengmustafaalan@users.noreply.github.com> Co-authored-by: aysenurkayabasi <aysenurkayabasi@users.noreply.github.com> Co-authored-by: bekirakbasoglu <bekirakbasoglu@users.noreply.github.com> Co-authored-by: ahmetahir <ahmetahir@users.noreply.github.com> Co-authored-by: myprivatecodecloud <myprivatecodecloud@users.noreply.github.com> Co-authored-by: bnturgut <bnturgut@users.noreply.github.com> Co-authored-by: talhasaglam <talhasaglam@users.noreply.github.com> Co-authored-by: ozgeaydiner23 <ozgeaydiner23@users.noreply.github.com> Co-authored-by: koksaleravci <koksaleravci@users.noreply.github.com> Co-authored-by: mehmet özcan <mehmetozcan1994@hotmail.com>

---
+++ secim.py
@@ -0,0 +1,4 @@
+## TODO burada liste eklenecek
+liste = ["Fatih","Gokhan","Mehmet","Ayşenur","Tahir","Banu","Bekir","Emre","Köksal","Mustafa","Talha","Özge","Özlem","İbrahim"]
+import random as rnd
+print(rnd.choice(liste))
