fbaf178b4fb8a4a6438dbabb8bfc2305e1541fc3
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2022-06-28 10:43:37 +0300

    Co-authored-by: Özlem Aslan <ozlemaslan@users.noreply.github.com> Co-authored-by: Fatih <fmetin@users.noreply.github.com> Co-authored-by: Mustafa Alan <cengmustafaalan@users.noreply.github.com> Co-authored-by: aysenurkayabasi <aysenurkayabasi@users.noreply.github.com> Co-authored-by: bekirakbasoglu <bekirakbasoglu@users.noreply.github.com> Co-authored-by: ahmetahir <ahmetahir@users.noreply.github.com> Co-authored-by: myprivatecodecloud <myprivatecodecloud@users.noreply.github.com> Co-authored-by: bnturgut <bnturgut@users.noreply.github.com> Co-authored-by: talhasaglam <talhasaglam@users.noreply.github.com> Co-authored-by: ozgeaydiner23 <ozgeaydiner23@users.noreply.github.com>

--- ornek.py
+++ ornek.py
@@ -1,11 +1,11 @@
 ## TODO burada liste eklenecek
 liste = ["Fatih","Gokhan","Mehmet","Ayşenur","Tahir","Banu","Bekir","Emre","Köksal","Mustafa","Talha","Özge","Özlem","İbrahim"]
 folderName = "Egzersizler"
-fileName = "podEgzersiz.txt" 
+fileName = "Pod" 
 import os
 for item in liste:
     if not os.path.exists(f"Egzersizler/{item}"):
         os.makedirs(f"Egzersizler/{item}")
-    with open(f"Egzersizler/{item}/{fileName}", "w") as f:
+    with open(f"Egzersizler/{item}/{item}_{fileName}.yaml", "w") as f:
         f.write(f"{item}")
     print(f"{item} klasörü oluşturuldu")
