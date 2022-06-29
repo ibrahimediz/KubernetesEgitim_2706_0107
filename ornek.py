eefa713aa08c0450ab534af9933568e417df0558
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2022-06-29 10:43:30 +0300

    29082022

--- ornek.py
+++ ornek.py
@@ -1,13 +1,18 @@
 ## TODO burada liste eklenecek
 liste = ["Fatih","Gokhan","Mehmet","Ayşenur","Tahir","Banu","Bekir","Emre","Köksal","Mustafa","Talha","Özge","Özlem","İbrahim"]
 folderName = "Egzersizler"
-fileName = "deployment" 
+fileName = "deploymentEgzersiz" 
+dosyatip = 2
 import os
 for item in liste:
     if not os.path.exists(f"Egzersizler/{item}"):
         os.makedirs(f"Egzersizler/{item}")
-    with open(f"Egzersizler/{item}/{item}_{fileName}.yaml", "x") as f:
-        f.write(f"# {item}")
+    if dosyatip == 1:
+        with open(f"Egzersizler/{item}/{item}_{fileName}.yaml", "x") as f:
+            f.write(f"# {item}")
+    else:
+        with open(f"Egzersizler/{item}/{item}_{fileName}.txt", "x") as f:
+            f.write(f"# {item}")
     print(f"{item} klasörü oluşturuldu")
 
 
